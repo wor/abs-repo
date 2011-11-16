87ff3adbe6055181df65dbca63a162f0b3b78ee0
Esa Määttä <esa.maatta_at_iki.fi>
2011-11-16 20:34:16 +0200

    mpd: fixed db caching on startup

--- mpd/mpd.conf.d
+++ mpd/mpd.conf.d
@@ -5,15 +5,15 @@ _mpd_org="/home/wor/.mpd/mpd.db"
 
 if ck_daemon mpd; then 
     # mpd stopped -> to start
-    if [[ -f ${_mpd_org} && $1 == start ]]; then
-        echo "caching mpd.db $1"
+    if [[ ! -L ${_mpd_org} && $1 == start ]]; then
+        echo "  Caching mpd.db $1"
         mv "${_mpd_org}" "${_mpd_cached}" || exit 1
         ln -s "${_mpd_cached}" "${_mpd_org}" || exit 1
     fi
 else
     # mpd running -> to stop
     if [[ -e ${_mpd_cached} && -L ${_mpd_org} && $1 == stop ]]; then
-        echo "uncaching mpd.db $1"
+        echo "  Uncaching mpd.db $1"
         mv "${_mpd_cached}" "${_mpd_org}"  
     fi
 fi
